2ed684f7308c5765eba09ae07fed4d64156aa4ec
Dmitry Bychenko <Zwerg.CopperNickel@gmail.com>
2023-05-19 22:51:19 +0100

    Login & Password

--- HostedCliTemplate/Settings.cs
+++ HostedCliTemplate/Settings.cs
@@ -4,5 +4,23 @@
 /// Class to contains Application Settings
 /// </summary>
 public sealed class Settings {
+  #region Public
+
+  /// <summary>
+  /// Login
+  /// </summary>
+  public string Login { get; init; } = "";
+
+  /// <summary>
+  /// Password
+  /// </summary>
+  public string Password { get; init; } = "";
+
+  /// <summary>
+  /// Bearer
+  /// </summary>
+  public string Bearer { get; init; } = "";
+
+  #endregion Public
 }
 
